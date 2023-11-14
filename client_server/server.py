625486c79b5e18a7aeeae45717cff7b2fbf60d64
Tag <saprx22@list.ru>
2023-11-14 21:08:01 +0500

    add python client server project

---
+++ client_server/server.py
@@ -0,0 +1,76 @@
+#!/bin/python3
+import socket
+import threading
+
+# Connection Data
+host = ''
+port = 55555
+
+def is_not_blank(s):
+    return bool(s and not s.isspace())
+
+def getserverip(server=""):
+    ip_address = ""
+    if is_not_blank(host) and 7 <= len(host) <= 15:
+        ip_address = server
+    else:
+        ip_address = socket.gethostbyname(socket.getfqdn())
+        # print(ip_address)
+    return ip_address
+
+
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((getserverip(host), port))
+server.listen()
+
+# Lists For Clients and Their Nicknames
+clients = []
+nicknames = []
+
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('utf-8'))
+            nicknames.remove(nickname)
+            break
+
+# Receiving / Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('utf-8'))
+        client.send('Connected to server!'.encode('utf-8'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server if listening...")
+receive()
